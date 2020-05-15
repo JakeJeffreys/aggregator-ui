cc3ed160dddee99e902cb644de7d2f04e814605c
JakeJeffreys <njjeffreys@gmail.com>
2020-05-15 14:00:43 -0600

    Testing api url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,13 @@
-export const API_BASE_URL = 'http://localhost:8080';
+let api_base_url = null;
+if (process.env.API_ENV === 'live')
+    api_base_url = "https://blogbase-app.herokuapp.com"
+else
+    api_base_url = 'http://localhost:8080';
+
+console.log(process.env.API_ENV);
+console.log(api_base_url);
+export const API_BASE_URL = api_base_url;
+
 export const ACCESS_TOKEN = 'accessToken';
 
 export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
